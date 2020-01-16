584300ba3a922021fd522bd18efdb4d4fc92cc0b
Fırat Sezel <firat.sezel@gmail.com>
2020-01-16 14:25:23 +0300

    white screen error fixed

---
+++ precache-manifest.4fb8b4f7151a6f9beaf2f329b9db617f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9803dbe5e646db3f2ede20d2c686e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d61671687a551256f00d",
+    "url": "./static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "d61671687a551256f00d",
+    "url": "./static/js/2.49ca3b20.chunk.js"
+  },
+  {
+    "revision": "33939b6cd19ef3b0579f9ccec9e0d2a2",
+    "url": "./static/js/2.49ca3b20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9d523545958d6f886df",
+    "url": "./static/js/main.7837b147.chunk.js"
+  },
+  {
+    "revision": "55c7f1c03a8f4de012f5",
+    "url": "./static/js/runtime-main.842e90e5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d0a144d6e309130077728268dca96be.js"
+  "./precache-manifest.4fb8b4f7151a6f9beaf2f329b9db617f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
