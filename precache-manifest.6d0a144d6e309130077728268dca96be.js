a584f1752ad6a9d5c49e1eed3d52763487b5a2ce
Fırat Sezel <firat.sezel@gmail.com>
2020-01-16 12:56:19 +0300

    initial commits

---
+++ precache-manifest.6d0a144d6e309130077728268dca96be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8726ea20a8b0563ba9949758f774b836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22c58bcb0af5959e5125",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "22c58bcb0af5959e5125",
+    "url": "/static/js/2.03c2567a.chunk.js"
+  },
+  {
+    "revision": "141f9337bb453df77b1efc37198d067a",
+    "url": "/static/js/2.03c2567a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1ec6540675c8c129748",
+    "url": "/static/js/main.feb24d32.chunk.js"
+  },
+  {
+    "revision": "35bfa7dfb94db9ad18e1",
+    "url": "/static/js/runtime-main.9f755098.js"
+  }
+]);
